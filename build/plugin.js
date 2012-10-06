b195529d0454a256c972aed950aeacfdbbe967e1
Alex Robson <asrobson@gmail.com>
2012-10-06 00:59:37 -0500

    Minor fixes to plugin and specs to get testing against harness in 0.8.12 working

--- build/plugin.js
+++ build/plugin.js
@@ -31,7 +31,7 @@ var jshintFactory = function( _, anvil ) {
 		inclusive: false,
 		exclusive: false,
 		breakBuild: true,
-		ignore: null,
+		ignore: [],
 		fileList: [],
 		commander: [
 			[ "-hint, --jshint", "JSHint all JavaScript files" ]
@@ -40,8 +40,6 @@ var jshintFactory = function( _, anvil ) {
 		messageFormat: new MessageFormat( "en" ),
 
 		configure: function( config, command, done ) {
-			console.log( "command: " + JSON.stringify( command ) );
-
 			if ( !_.isEmpty( this.config ) ) {
 				if ( this.config.all ) {
 					this.all = true;
@@ -84,8 +82,6 @@ var jshintFactory = function( _, anvil ) {
 				totalErrors = 0,
 				transforms, message;
 
-			console.log( "BEGIN run()" );
-
 			if ( this.inclusive ) {
 				jsFiles = _.filter( anvil.project.files, this.anyFile( this.fileList ) );
 			} else if ( this.all || this.exclusive ) {
@@ -96,12 +92,6 @@ var jshintFactory = function( _, anvil ) {
 					jsFiles = _.reject( jsFiles, this.anyFile( this.fileList ) );
 				}
 			}
-
-			console.log( "inclusive: " + this.inclusive );
-			console.log( "all: " + this.all );
-			console.log( "exclusive: " + this.exclusive );
-			console.log( "anvil.project.files: " + JSON.stringify( anvil.project.files ) );
-			console.log( "jsFiles.length: " + jsFiles.length );
 
 			if ( jsFiles.length > 0 ) {
 				message = this.messageFormat.compile( "Linting {NUM_RESULTS, plural, one{one file} other{# files}}." );
@@ -160,7 +150,7 @@ var jshintFactory = function( _, anvil ) {
 				anvil.log.event( "No issues Found." );
 				that.log( "No issues Found." );
 			} else {
-				validErrors = this.processErrors( file, jshint.errors, this.ignore );
+				validErrors = this.processErrors( file, jshint.errors, this.ignore || [] );
 				_.each( validErrors, function( error ) {
 					anvil.log.error( error );
 					that.log( error );
